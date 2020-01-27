123e05bc832019b12f09446276d2c44934c12f66
alexterua <alexter.od.ua@gmail.com>
2020-01-27 21:17:51 +0200

    Create model Article and migration for her

---
+++ database/migrations/2020_01_27_185720_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('author_id')->unsigned();
+            $table->string('title');
+            $table->string('short_title');
+            $table->text('text');
+            $table->string('img')->nullable();
+            $table->timestamps();
+
+            $table->foreign('author_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
