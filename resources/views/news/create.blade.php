977df35ed6f044a4f582b7c4f137190c97b1803b
alexterua <alexter.od.ua@gmail.com>
2020-01-29 22:19:49 +0200

    edit template create

--- resources/views/news/create.blade.php
+++ resources/views/news/create.blade.php
@@ -3,18 +3,23 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <h2 style="margin: 0 auto;">Создание новости</h2>
+            <h2 style="margin: 2px auto;">Создание новости</h2>
         </div>
         <div class="row">
 
-            <form action="" method="post" style="width: 60%; margin-left: 20%;">
+            <form action="" method="post" enctype="multipart/form-data" style="width: 60%; margin-left: 20%;">
+                @csrf
                 <div class="form-group">
                     <label for="title">Заголовок</label>
                     <input id="title" class="form-control" type="text" placeholder="Введите название...">
                 </div>
                 <div class="form-group">
                     <label for="text">Содержание новости</label>
-                    <textarea class="form-control" id="text" rows="10" placeholder="Введите текст..."></textarea>
+                    <textarea class="form-control" id="text" rows="8" placeholder="Введите текст..."></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="img">Выберите изображение</label>
+                    <input type="file" class="form-control-file" id="img">
                 </div>
                 <button type="submit" class="btn btn-primary">Сохранить</button>
             </form>
